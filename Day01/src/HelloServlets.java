535fdc48c6839345e1efc32b36ba2c3e24017975
sandula-sanchana <sandulasanchana@gmail.com>
2025-11-13 13:50:04 +0530

    printWriter

--- Day01/src/HelloServlets.java
+++ Day01/src/HelloServlets.java
@@ -4,10 +4,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+//        super.doGet(req, resp);
+
+        System.out.println("Hello Servlets");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello Servlets</h1>");
     }
 }
