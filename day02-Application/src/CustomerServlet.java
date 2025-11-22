27c9f89199e8e18647ddd182fbd2fdeecfebcd9f
sandula-sanchana <sandulasanchana@gmail.com>
2025-11-22 13:24:50 +0530

    customer put done

--- day02-Application/src/CustomerServlet.java
+++ day02-Application/src/CustomerServlet.java
@@ -12,41 +12,49 @@ import java.util.ArrayList;
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
-    ArrayList<CustomerDto> customerDtos=new ArrayList<>();
+    public  static ArrayList<CustomerDto> customers = new ArrayList<>();
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("customer doPost");
-        String id=req.getParameter("id");
-        System.out.println("customer id:"+id);
-        String name=req.getParameter("name");
-        System.out.println("customer name:"+name);
-        String address=req.getParameter("address");
-        System.out.println("customer address:"+address);
+       String id= req.getParameter("id");
+       String name= req.getParameter("name");
+       String addr= req.getParameter("address");
 
-        customerDtos.add(new CustomerDto(id,name,address));
+       customers.add(new CustomerDto(id,name,addr));
 
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html;charset=utf-8");
-        PrintWriter out = resp.getWriter();
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out=resp.getWriter();
 
-        out.println("<table border='1'>");
-        out.println("<tr><th>#</th><th>ID</th><th>Name</th><th>Address</th></tr>");
-        int index = 1;
-        for(CustomerDto customerDto:customerDtos){
-            System.out.println(customerDto);
-            out.println(
-                    "<tr>" +
-                            "<td>" + index + "</td>" +
-                            "<td>" + customerDto.getId() + "</td>" +
-                            "<td>" + customerDto.getName() + "</td>" +
-                            "<td>" + customerDto.getAddress() + "</td>" +
-                            "</tr>"
-            );
-            index++;
+        for(CustomerDto c:customers){
+            out.println("<tr>"+"<td>"+c.getId()+"</td>"+"<td>"+c.getName()+"</td>"+"<td>"+c.getAddress()+"</td>"+"</tr>");
         }
-        out.println("</table>");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id").trim();
+        String name = req.getParameter("name");
+        String addr = req.getParameter("address");
+        System.out.println(customers.size());
+        boolean updated = false;
+
+        for (CustomerDto c : customers) {
+            if (c.getId().equals(id)) {
+                c.setName(name);
+                c.setAddress(addr);
+                updated = true;
+                System.out.println(customers.size());
+                System.out.println(c);
+                break;
+            }
+        }
+
+        if (!updated) {
+            customers.add(new CustomerDto(id, name, addr));
+            //System.out.println(customers.size());
+        }
     }
 }
