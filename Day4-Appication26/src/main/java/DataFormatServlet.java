7963d799d28e5ad4ddafa6615ea044d82cc2dc0a
sandula-sanchana <sandulasanchana@gmail.com>
2025-12-18 09:31:15 +0530

    add data formats servlets

---
+++ Day4-Appication26/src/main/java/DataFormatServlet.java
@@ -0,0 +1,16 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/data-formats")
+public class DataFormatServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String contentType = req.getContentType();
+        System.out.println("contentType:"+contentType);
+    }
+}
