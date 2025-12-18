5f73bb54124c6425b698f68e1573751405bb800a
sandula-sanchana <sandulasanchana@gmail.com>
2025-12-18 10:03:48 +0530

    form data

--- Day4-Appication26/src/main/java/DataFormatServlet.java
+++ Day4-Appication26/src/main/java/DataFormatServlet.java
@@ -1,4 +1,5 @@
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,12 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/data-formats")
+@MultipartConfig
 public class DataFormatServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String contentType = req.getContentType();
         System.out.println("contentType:"+contentType);
-        System.out.println(req.getParameter("name"));//for query param
+//        System.out.println(req.getParameter("name"));//for query param
 
         /// /////////////////////////////////////////////////////////////////
 
@@ -20,6 +22,7 @@ public class DataFormatServlet extends HttpServlet {
 
         /// //////////////////////////////////////////////////////////////
 
-        System.out.println(req.getParameter("age"));
+        System.out.println(req.getParameter("age"));//form data// u need @multiplepartconfig annotation
+        System.out.println(req.getParameter("image"));
     }
 }
