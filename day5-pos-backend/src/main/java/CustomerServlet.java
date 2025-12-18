7c43653ae52a26d796ceb02ad82bf75bbab3d1f3
sandula-sanchana <sandulasanchana@gmail.com>
2025-12-18 15:18:26 +0530

    add context listener and dbcp

--- day5-pos-backend/src/main/java/CustomerServlet.java
+++ day5-pos-backend/src/main/java/CustomerServlet.java
@@ -1,22 +1,56 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 @WebServlet(urlPatterns = "/api/v1/customer")
 public class CustomerServlet extends HttpServlet {
+    BasicDataSource ds;
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("do post pos");
-        Gson gson = new Gson();
-        JsonObject jsonObject =gson.fromJson(req.getReader(), JsonObject.class);
-        System.out.println("jsonObject:"+jsonObject);
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+        ds=(BasicDataSource)servletContext.getAttribute("ds");
+
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(req.getReader(), JsonObject.class);
+
+        int id = jsonObject.get("id").getAsInt();
+        String name = jsonObject.get("name").getAsString();
+        String addr = jsonObject.get("address").getAsString();
+
+        String sql = "INSERT INTO customer VALUES (?,?,?)";
+
+        try (Connection con = ds.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ps.setString(2, name);
+            ps.setString(3, addr);
+
+            resp.getWriter().println(
+                    ps.executeUpdate() > 0 ? "Customer Saved" : "Insert Failed"
+            );
+
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
