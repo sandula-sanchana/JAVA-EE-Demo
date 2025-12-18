5704864e754d06304bb4750e48a25cd957ffabf2
sandula-sanchana <sandulasanchana@gmail.com>
2025-12-18 11:57:17 +0530

    day 5 pos project-backend+frontend----do Post

---
+++ day5-pos-backend/src/main/java/CustomerServlet.java
@@ -0,0 +1,20 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/api/v1/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do post pos");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do get pos");
+    }
+}
